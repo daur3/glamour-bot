6c8e8a27ad0ff4b6b3b62d3acc881b77066e2957
daur3 <141478037+daur3@users.noreply.github.com>
2026-07-24 15:48:32 +0500

    Update botai.py

--- botai.py
+++ botai.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 from groq import AsyncGroq
